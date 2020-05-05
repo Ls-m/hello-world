ca905f9fcc2a6e195ee0be958951bd8a1db8711a
Lham <elham.fr80@gmail.com>
2020-05-05 18:53:24 +0430

    a new sout added

--- Simple.java
+++ Simple.java
@@ -1,5 +1,6 @@
 public class Simple{
     public static void main(String[] args) {
         System.out.println("this is a simple program");
+        System.out.println("first modification");
     }
 }
